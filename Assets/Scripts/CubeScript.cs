4e3e7ed6c3fd9e3388552c16e0472718a679dde0
sharto66 <sean.hartnett@student.dit.ie>
2016-05-23 19:29:40 +0100

    Initial commit. A basic working prototype

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+    bool gravitySwitch = false;
+	
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down * 10 * Time.deltaTime);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (gravitySwitch)
+            {
+                GetComponent<Rigidbody2D>().gravityScale = -1.0f;
+                gravitySwitch = false;
+            }
+            else
+            {
+                GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+                gravitySwitch = true;
+            }
+            
+        }
+	}
+}
---
+++ Assets/Scripts/FlyingObjectScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyingObjectScript : MonoBehaviour {
+
+    GameObject player, camera;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindWithTag("Player");
+        camera = GameObject.FindWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.left * 5 * Time.deltaTime);
+        float distance = Vector2.Distance((Vector2)player.transform.position, (Vector2)transform.position);
+        if (distance >= 20)
+        {
+            int yAxis = Random.Range(-3, 4);
+            Vector3 spawnLoc = camera.transform.position + (Vector3.right * 10) + (Vector3.forward * 10) + (Vector3.up * yAxis);
+            Object obj = Instantiate(gameObject, spawnLoc, Quaternion.identity);
+            obj.name = obj.name.Replace("(Clone)", "");
+            Destroy(this.gameObject);
+        }
+	}
+}
